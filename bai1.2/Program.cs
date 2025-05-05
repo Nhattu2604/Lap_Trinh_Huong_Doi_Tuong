786999a587aad9c26c517fb545da95a5f4478b21
Nguyễn Nhật Tú <152679635+Nhattu2604@users.noreply.github.com>
2025-05-05 16:37:25 +0700

    Add files via upload

---
+++ bai1.2/Program.cs
@@ -0,0 +1,50 @@
+﻿/*
+Viết chương trình nhập từ bàn phím hai số nguyên a, b và một ký tự ch.
+Nếu:
+- ch là "+" thì thực hiện phép tính a + b và in kết quả lên màn hình.
+- ch là "–" thì thực hiện phép tính a - b và in kết quả lên màn hình.
+- ch là "*" thì thực hiện phép tính a * b và in kết quả lên màn hình.
+- ch là "/" thì thực hiện phép tính a / b và in kết quả lên màn hình. 
+Lưu ý kiểm tra điều kiện của phép chia. Nếu b == 0 thì in ra thông báo "Error!"
+*/
+
+using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a,b;
+            char phepToan;
+            double ketQua=0;
+            
+            // Input: Không thay đổi phần này
+            Console.Write("Nhap a = ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Nhap b = ");
+            b = int.Parse(Console.ReadLine());
+            Console.Write("Nhap phep toan (+,-,*,/): ");
+            phepToan = char.Parse(Console.ReadLine());
+            if(phepToan =='+'){
+                ketQua = a + b;
+            }
+            else if(phepToan == '-'){
+                ketQua = a - b;
+            }
+             else if(phepToan == '*'){
+                ketQua = a * b;
+            }
+             else if(phepToan == '/'){
+                ketQua = a / b;
+            }
+            else {
+                Console.WriteLine("Phep toan khong hop le!");
+            }
+
+            Console.WriteLine("KetQua: {0}",ketQua);
+
+        }
+    }
+}
