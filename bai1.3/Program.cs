814f5a907423c71e4cdbf54b68994becf37e1579
Nguyễn Nhật Tú <152679635+Nhattu2604@users.noreply.github.com>
2025-05-05 16:43:18 +0700

    Add files via upload

---
+++ bai1.3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class Program{
+    static void Main() {
+        Console.Write("Nhap so luong vi trung ban dau: ");
+        int n = int.Parse(Console.ReadLine());
+        Console.Write("Nhap so gio: ");
+        int h = int.Parse(Console.ReadLine());
+        long S = n * (long)Math.Pow(2,h);
+        Console.WriteLine($"So luong vi trung nhan len {h} la: {S} ");
+    }
+
+
+
+}
